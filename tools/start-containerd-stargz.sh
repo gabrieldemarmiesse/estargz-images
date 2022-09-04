1f19dce045a444affb10a5b34e65470e8ee8e29b
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2022-09-04 13:00:28 +0000

    Increase the amount of time to start containerd

--- tools/start-containerd-stargz.sh
+++ tools/start-containerd-stargz.sh
@@ -3,7 +3,7 @@ set -e
 containerd-stargz-grpc > /dev/null 2>&1 &
 containerd > /dev/null 2>&1 &
 
-sleep 5
+sleep 10
 
 # we verify that it works
 nerdctl --snapshotter=stargz run ghcr.io/stargz-containers/python:3.10-esgz python -c "print('hello-world')"
