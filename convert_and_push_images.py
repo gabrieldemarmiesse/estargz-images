afb4238cd70fcdea8052d5ab52206c4472140983
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2022-09-04 13:51:43 +0000

    Simplify quoting

--- convert_and_push_images.py
+++ convert_and_push_images.py
@@ -4,7 +4,7 @@ CONVERTED_IMAGES_PREFIX = "docker.io/gabrieldemarmiesse/"
 COMPRESSION_LEVEL = 6
 
 
-def convert_and_push_one_image(docker_image_name: str):
+def convert_and_push(docker_image_name: str, entrypoint=None, args=None):
     subprocess.check_call(["nerdctl", "pull", docker_image_name])
 
     if "/" not in docker_image_name:
@@ -13,6 +13,12 @@ def convert_and_push_one_image(docker_image_name: str):
         normalized_docker_image_name = docker_image_name
 
     estargz_docker_image_name = CONVERTED_IMAGES_PREFIX + docker_image_name
+
+    additional_options = []
+    if entrypoint:
+        additional_options += ["--entrypoint", entrypoint]
+    if args:
+        additional_options += ["--args", args]
     subprocess.check_call(
         [
             "ctr-remote",
@@ -24,9 +30,19 @@ def convert_and_push_one_image(docker_image_name: str):
             normalized_docker_image_name,
             estargz_docker_image_name,
         ]
+        + additional_options
     )
     subprocess.check_call(["nerdctl", "push", estargz_docker_image_name])
+    subprocess.check_call(
+        ["nerdctl", "image", "rm", docker_image_name, estargz_docker_image_name]
+    )
     return estargz_docker_image_name
 
 
-convert_and_push_one_image("python:3.7")
+convert_and_push("python:3.7", entrypoint='["python", "-c", "print(\'hello world\')"]')
+convert_and_push("python:3.8", entrypoint='["python", "-c", "print(\'hello world\')"]')
+convert_and_push("python:3.9", entrypoint='["python", "-c", "print(\'hello world\')"]')
+convert_and_push("python:3.10", entrypoint='["python", "-c", "print(\'hello world\')"]')
+convert_and_push(
+    "python:3.10-slim", entrypoint='["python", "-c", "print(\'hello world\')"]'
+)
