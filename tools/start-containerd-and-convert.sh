f75c4aaf63c3350f1d76ee3f8216dd75081ae982
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2022-09-18 19:15:42 +0000

    Remove debug code

--- tools/start-containerd-and-convert.sh
+++ tools/start-containerd-and-convert.sh
@@ -11,5 +11,4 @@ nerdctl run hello-world
 # login and start converting
 echo $GHCR_TOKEN | nerdctl login ghcr.io -u $ --password-stdin
 echo $GHCR_TOKEN | crane auth login -u $ --password-stdin ghcr.io
-crane manifest ghcr.io/gabrieldemarmiesse/estargz-images/alpine:3.15.3-org
 python -u ./convert_and_push_images.py
