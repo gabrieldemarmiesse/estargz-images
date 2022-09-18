d06d7239e237b6e8eec2f2985d00a1010351a70a
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2022-09-18 18:43:05 +0000

    Put all logic in shell script

---
+++ tools/start-containerd-and-convert.sh
@@ -0,0 +1,15 @@
+set -eux
+
+containerd -v
+containerd &
+
+sleep 5
+
+# we verify that it works
+nerdctl run hello-world
+
+# login and start converting
+echo $GITHUB_TOKEN | nerdctl login ghcr.io -u $ --password-stdin
+echo $GITHUB_TOKEN | crane auth login -u $ --password-stdin ghcr.io
+crane manifest ghcr.io/gabrieldemarmiesse/estargz-images/alpine:3.15.3-org
+python -u ./convert_and_push_images.py
--- tools/start-containerd.sh
+++
@@ -1,9 +0,0 @@
-set -eux
-
-containerd -v
-containerd &
-
-sleep 5
-
-# we verify that it works
-nerdctl run hello-world
